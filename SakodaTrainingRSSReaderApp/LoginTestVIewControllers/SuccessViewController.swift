6fddf84f712159ba9da265d830d5e8bf4e4b1581
sasasan03 <sako.hiro62@gmail.com>
2024-06-18 18:54:22 +0900

    add: テスト用のViewを追加

---
+++ SakodaTrainingRSSReaderApp/LoginTestVIewControllers/SuccessViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SuccessViewController.swift
+//  SakodaTrainingRSSReaderApp
+//
+//  Created by sako0602 on 2024/06/18.
+//
+
+import UIKit
+
+class SuccessViewController: UIViewController {
+    static let storyboard = "SuccessView"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
