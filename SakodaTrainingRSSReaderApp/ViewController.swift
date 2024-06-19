7dce2f3b21d819780b734d62d18d43097b871c02
sasasan03 <sako.hiro62@gmail.com>
2024-06-19 19:17:06 +0900

    chage: FirebaseClientの名前を変更。-> FirebaseAuthClient

--- SakodaTrainingRSSReaderApp/ViewController.swift
+++ SakodaTrainingRSSReaderApp/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let client = FirebaseClient()
+        let client = FirebaseAuthClient()
         GoogleSignInButton.addAction(UIAction { _ in
             Task {
                 do {
