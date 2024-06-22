dd506abba9896cffec9dcb6b590e6878b59a50ff
sasasan03 <sako.hiro62@gmail.com>
2024-06-22 18:00:51 +0900

    change: SceanDelegateを使用して遷移させるためのサンプルコードを追加

--- SakodaTrainingRSSReaderApp/SceneDelegate.swift
+++ SakodaTrainingRSSReaderApp/SceneDelegate.swift
@@ -11,14 +11,28 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        let client = FirebaseClient()
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            print("#")
+            if client.authenticationState == .authenticated {
+                // ログインしている場合、successViewをルートビューコントローラとして設定
+                let successViewController = storyboard.instantiateViewController(identifier: SuccessViewController.storyboardID)
+                window.rootViewController = successViewController
+            } else {
+                // ログインしていない場合、ログイン画面をルートビューコントローラとして設定
+                let mainViewController = storyboard.instantiateViewController(identifier: ViewController.storyBoardID)
+                window.rootViewController = mainViewController
+            }
+            print("#window1")
+            self.window = window
+            window.makeKeyAndVisible()
+            print("#window2")
+        }
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
