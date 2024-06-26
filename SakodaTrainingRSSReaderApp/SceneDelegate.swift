375d30af5aeda733a8c17213ce0906b1b425c962
sasasan03 <sako.hiro62@gmail.com>
2024-06-26 19:43:10 +0900

    change: SceneDelegateのsceneメソッドを元の状態に変更

--- SakodaTrainingRSSReaderApp/SceneDelegate.swift
+++ SakodaTrainingRSSReaderApp/SceneDelegate.swift
@@ -12,23 +12,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        let client = FirebaseClient()
-        if let windowScene = scene as? UIWindowScene {
-            let window = UIWindow(windowScene: windowScene)
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            if client.authenticationState == .authenticated {
-                // ログインしている場合、successViewをルートビューコントローラとして設定
-                let successViewController = storyboard.instantiateViewController(identifier: SuccessViewController.storyboardID)
-                window.rootViewController = successViewController
-            } else {
-                // ログインしていない場合、ログイン画面をルートビューコントローラとして設定
-                let mainViewController = storyboard.instantiateViewController(identifier: ViewController.storyBoardID)
-                window.rootViewController = mainViewController
-            }
-            self.window = window
-            window.makeKeyAndVisible()
+            // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+            // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+            // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+            guard let _ = (scene as? UIWindowScene) else { return }
         }
-    }
     
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
