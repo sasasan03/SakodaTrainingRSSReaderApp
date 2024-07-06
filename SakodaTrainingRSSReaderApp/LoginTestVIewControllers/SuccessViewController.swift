f0aa41f589ecaa77a9be62bfafce952797c91a58
sasasan03 <sako.hiro62@gmail.com>
2024-07-06 16:28:38 +0900

    add: ユーザー情報を削除するメソッドを追加

--- SakodaTrainingRSSReaderApp/LoginTestVIewControllers/SuccessViewController.swift
+++ SakodaTrainingRSSReaderApp/LoginTestVIewControllers/SuccessViewController.swift
@@ -9,7 +9,13 @@ import UIKit
 
 class SuccessViewController: UIViewController {
     static let storyboardID = "SuccessView"
+    let firebaseClient = FirebaseClient()
+    let userDefaultManager = UserDefaultsManager()
     
+    @IBAction func signOut(_ sender: Any) {
+        firebaseClient.signOut()
+        userDefaultManager.delete()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
