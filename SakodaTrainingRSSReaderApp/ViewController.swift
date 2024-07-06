87513bd6cc6e0db88246e8e9ee2d380449d279fd
sasasan03 <sako.hiro62@gmail.com>
2024-07-06 17:06:21 +0900

    change: 初回ログイン時とそれ以降のログイン時で遷移するViewを変更

--- SakodaTrainingRSSReaderApp/ViewController.swift
+++ SakodaTrainingRSSReaderApp/ViewController.swift
@@ -18,22 +18,12 @@ class ViewController: UIViewController {
     static let storyBoardID = "Main"
     let client = FirebaseClient()
     let userDefaultsManager = UserDefaultsManager()
+    var userID: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // ユーザーが登録状況取得
-        client.registerAuthStateHandler()
+        userID = userDefaultsManager.userIDLoad()
         setupGoogleSignInButton()
-//        do {
-//            let authenticationState = try userDefaultsManager.loadCredentials()
-//            if authenticationState == .authenticated {
-//                navigateToSuccessViewController()
-//            } else {
-//                navigateToLoginViewController()
-//            }
-//        } catch {
-//            
-//        }
     }
     
     @IBOutlet weak var GoogleSignInButton: UIButton!
@@ -44,9 +34,20 @@ class ViewController: UIViewController {
                 do {
                     let signInResult = try await self.client.signIn()
                     if signInResult {
-                        guard let successView = self.storyboard?.instantiateViewController(withIdentifier: SuccessViewController.storyboardID) else { return print("#with Identifier miss") }
-                        self.navigationController?.pushViewController(successView, animated: true)
-                        try self.userDefaultsManager.saveCredentials(authenticationState: .authenticated)
+                        // 初回ログイン時とそれ以降のログインの遷移を管理。
+                        if self.userID == nil {
+                            guard let successView = self.storyboard?.instantiateViewController(
+                                withIdentifier: SuccessViewController.storyboardID) else {
+                                return print("?? The specified Storyboard cannot be found.(SuccessViewController.storyboardID)")
+                            }
+                            self.navigationController?.pushViewController(successView, animated: true)
+                        } else {
+                            guard let rssFeedSelectionVC = self.storyboard?.instantiateViewController(
+                                withIdentifier: RSSFeedSelectionViewController.storyboardID) else {
+                                return print("?? The specified Storyboard cannot be found.(UserSignUpViewController.storyboardID)")
+                            }
+                            self.navigationController?.pushViewController(rssFeedSelectionVC, animated: true)
+                        }
                     } else {
                         print("#signn  dissmiss")
                     }
@@ -56,39 +57,4 @@ class ViewController: UIViewController {
             }
         }, for: .touchUpInside)
     }
-
-    
-    private let storyboardInstance = UIStoryboard(name: "Main", bundle: nil)
-    
-    private func navigateToSuccessViewController(){
-        if let successViewController = storyboardInstance.instantiateViewController(withIdentifier: SuccessViewController.storyboardID) as? SuccessViewController {
-            let navigationController =  UINavigationController(rootViewController: successViewController)
-            setRootViewController(navigationController)
-        }
-    }
-    
-    private func navigateToLoginViewController(){
-        if let loginViewController = storyboardInstance.instantiateViewController(withIdentifier: LoginViewController.storyboardID) as? LoginViewController {
-            let navigationController =  UINavigationController(rootViewController: loginViewController)
-            setRootViewController(navigationController)
-        }
-    }
-
-    // アプリケーションのウィンドウのルートビューコントローラを設定し、表示する関数
-    private func setRootViewController(_ viewController: UIViewController) {
-        guard let windowScene = (UIApplication.shared.connectedScenes.first as? UIWindowScene),
-              let window = windowScene.windows.first else {
-            return
-        }
-        
-        window.rootViewController = viewController
-        window.makeKeyAndVisible()
-    }
-    
-    @IBAction func didTapToSignViewButton(_ sender: Any) {
-        let userSignUpViewController = self.storyboard?.instantiateViewController(withIdentifier: UserSignUpViewController.storyboardID) as! UserSignUpViewController
-        self.navigationController?.pushViewController(userSignUpViewController, animated: true)
-    }
-    
-    
 }
