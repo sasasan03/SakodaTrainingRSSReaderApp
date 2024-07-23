f4369e14f877887516f00966846dd8469252023b
sasasan03 <sako.hiro62@gmail.com>
2024-07-23 19:29:19 +0900

    feat: TopicのEntityを追加

---
+++ SakodaTrainingRSSReaderApp/Model/Data/TopicEntity.swift
@@ -0,0 +1,14 @@
+//
+//  TopicEntity.swift
+//  SakodaTrainingRSSReaderApp
+//
+//  Created by sako0602 on 2024/07/23.
+//
+
+import Foundation
+
+struct Topic: Codable, Identifiable, Equatable {
+    var id = UUID()
+    let title: String
+    let url: String
+}
