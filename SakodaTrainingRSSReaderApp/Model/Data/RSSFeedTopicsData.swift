d703c309a21e2921c18e7c80e532daf51eecf96b
sasasan03 <sako.hiro62@gmail.com>
2024-07-25 18:03:01 +0900

    feat: TopicEntityにBool値のisCheckedプロパティを追加

--- SakodaTrainingRSSReaderApp/Model/Data/RSSFeedTopicsData.swift
+++ SakodaTrainingRSSReaderApp/Model/Data/RSSFeedTopicsData.swift
@@ -9,14 +9,14 @@ import Foundation
 
 struct RSSFeedTopicsData {
     let topicsData = [
-        Topic(title: "主要", url: "https://news.yahoo.co.jp/rss/topics/top-picks.xml"),
-        Topic(title: "国内", url: "https://news.yahoo.co.jp/rss/media/domestic.xml"),
-        Topic(title: "国際", url: "https://news.yahoo.co.jp/rss/media/international.xml"),
-        Topic(title: "経済", url: "https://news.yahoo.co.jp/rss/media/economy.xml"),
-        Topic(title: "エンタメ", url: "https://news.yahoo.co.jp/rss/media/entertainment.xml"),
-        Topic(title: "スポーツ", url: "https://news.yahoo.co.jp/rss/media/sports.xml"),
-        Topic(title: "IT", url: "https://news.yahoo.co.jp/rss/media/it.xml"),
-        Topic(title: "科学", url: "https://news.yahoo.co.jp/rss/media/science.xml")
+        Topic(title: "主要", url: "https://news.yahoo.co.jp/rss/topics/top-picks.xml", isChecked: false),
+        Topic(title: "国内", url: "https://news.yahoo.co.jp/rss/media/domestic.xml", isChecked: false),
+        Topic(title: "国際", url: "https://news.yahoo.co.jp/rss/media/international.xml", isChecked: false),
+        Topic(title: "経済", url: "https://news.yahoo.co.jp/rss/media/economy.xml", isChecked: false),
+        Topic(title: "エンタメ", url: "https://news.yahoo.co.jp/rss/media/entertainment.xml", isChecked: false),
+        Topic(title: "スポーツ", url: "https://news.yahoo.co.jp/rss/media/sports.xml", isChecked: false),
+        Topic(title: "IT", url: "https://news.yahoo.co.jp/rss/media/it.xml", isChecked: false),
+        Topic(title: "科学", url: "https://news.yahoo.co.jp/rss/media/science.xml", isChecked: false)
     ]
 
 }
