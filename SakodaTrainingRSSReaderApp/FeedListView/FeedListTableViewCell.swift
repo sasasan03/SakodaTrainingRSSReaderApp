5567f1256eb7c5fa8c276f73c95610456f0f2f30
sasasan03 <sako.hiro62@gmail.com>
2024-07-26 22:13:17 +0900

    feat: cellの構築の実装を追加

--- SakodaTrainingRSSReaderApp/FeedListView/FeedListTableViewCell.swift
+++ SakodaTrainingRSSReaderApp/FeedListView/FeedListTableViewCell.swift
@@ -8,13 +8,25 @@
 import UIKit
 
 class FeedListTableViewCell: UITableViewCell {
-
+    
+    static let cellNibName = "FeedListTableViewCell"
+    static let cellIdentifier = "FeedListCell"
+    
+    @IBOutlet weak var feedText: UILabel!
+    @IBOutlet weak var favoriteImage: UIImageView!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+    
+    func configureCellContent(topic: Topic){
+        favoriteImage.image = UIImage(systemName: topic.isChecked ? "heart.fill" : "heart")
+        feedText.text = topic.title
     }
     
 }
