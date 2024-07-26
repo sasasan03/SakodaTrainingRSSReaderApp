03d00b322cc7e9e87aea48b28a2d86312532a8f9
sasasan03 <sako.hiro62@gmail.com>
2024-07-26 21:47:00 +0900

    add: cellを追加

---
+++ SakodaTrainingRSSReaderApp/FeedListView/FeedListTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  FeedListTableViewCell.swift
+//  SakodaTrainingRSSReaderApp
+//
+//  Created by sako0602 on 2024/07/26.
+//
+
+import UIKit
+
+class FeedListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
