af7f4dd03b090ced9e6c11acdaefccc586927841
sasasan03 <sako.hiro62@gmail.com>
2024-07-28 19:26:55 +0900

    feat:  WebKitを使ってViewを表示できるよう処理を追加

--- SakodaTrainingRSSReaderApp/ArticleView/ArticleViewController.swift
+++ SakodaTrainingRSSReaderApp/ArticleView/ArticleViewController.swift
@@ -6,11 +6,35 @@
 //
 
 import UIKit
+import WebKit
 
 class ArticleViewController: UIViewController {
-
+    
+    enum URLError: Error {
+        case invalidURL
+    }
+    
+    @IBOutlet weak var webView: WKWebView!
+    var urlString: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
     }
+    
+}
+
+extension ArticleViewController {
+    private func loadURL() throws {
+        guard let urlString = urlString, let url = URL(string: urlString) else {
+            throw URLError.invalidURL
+        }
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+}
+
+extension ArticleViewController: WKUIDelegate, WKNavigationDelegate {
     
 }
