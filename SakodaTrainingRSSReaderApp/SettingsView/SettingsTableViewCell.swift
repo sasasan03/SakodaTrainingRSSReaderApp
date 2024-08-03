7e84a7157769dfc08b8329005adceb2737a4903b
sasasan03 <sako.hiro62@gmail.com>
2024-08-03 16:24:45 +0900

    add: tabelViewCellを追加

---
+++ SakodaTrainingRSSReaderApp/SettingsView/SettingsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsTableViewCell.swift
+//  SakodaTrainingRSSReaderApp
+//
+//  Created by sako0602 on 2024/08/03.
+//
+
+import UIKit
+
+class SettingsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
