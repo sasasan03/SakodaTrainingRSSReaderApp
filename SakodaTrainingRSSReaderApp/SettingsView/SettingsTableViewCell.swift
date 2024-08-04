4344afe52647f13e303242e6f516d8fd58e854dd
sasasan03 <sako.hiro62@gmail.com>
2024-08-04 12:44:03 +0900

    feat: cellにidentifierを記述。Labelを追加

--- SakodaTrainingRSSReaderApp/SettingsView/SettingsTableViewCell.swift
+++ SakodaTrainingRSSReaderApp/SettingsView/SettingsTableViewCell.swift
@@ -9,15 +9,14 @@ import UIKit
 
 class SettingsTableViewCell: UITableViewCell {
 
+    static let Identifier = "SettingsfViewCell"
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
 }
