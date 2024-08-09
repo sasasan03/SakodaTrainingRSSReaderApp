1ec4964f19a249b6fe47596a2e5d7bbe375e9bed
sasasan03 <sako.hiro62@gmail.com>
2024-08-09 18:03:37 +0900

    feat: SettingsViewControllerが持つ項目プロパティをStringからenumの型へ変更

--- SakodaTrainingRSSReaderApp/SettingsView/SettingsViewController.swift
+++ SakodaTrainingRSSReaderApp/SettingsView/SettingsViewController.swift
@@ -8,15 +8,15 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
-    private let items = [
-        "一覧画面の表示切り替え",
-        "RSS取得間隔",
-        "購読RSS管理",
-        "文字サイズ変更",
-        "ダークモード",
-        "ログアウト"
-    ]
+    
+    enum SettingsOption: String, CaseIterable {
+        case toggleListView = "一覧画面の表示切り替え"
+        case rssInterval = "RSS取得間隔"
+        case manageRss = "購読RSS管理"
+        case changeFontSize = "文字サイズ変更"
+        case darkMode = "ダークモード"
+        case logout = "ログアウト"
+    }
     
     @IBOutlet weak var settingsTableView: UITableView!
     
@@ -34,7 +34,7 @@ class SettingsViewController: UIViewController {
 
 extension SettingsViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return SettingsOption.allCases.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -42,8 +42,35 @@ extension SettingsViewController: UITableViewDataSource, UITableViewDelegate {
             withIdentifier: SettingsTableViewCell.cellIdentifier,
             for: indexPath
         ) as! SettingsTableViewCell
-        cell.configureCellContent(text: items[indexPath.row])
+        cell.configureCellContent(text: SettingsOption.allCases[indexPath.row].rawValue)
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectItem = SettingsOption.allCases[indexPath.row]
+        
+        let destinationVC: UIViewController
+        switch selectItem {
+        case .toggleListView:
+            destinationVC = ToggleListViewController()
+        case .rssInterval:
+            destinationVC = RssIntervalViewController()
+        case .manageRss:
+            destinationVC = ManageRssViewController()
+        case .changeFontSize:
+            destinationVC = FontSizeChangeViewController()
+        case .darkMode:
+            destinationVC = DarkModeViewController()
+        case .logout:
+            //TODO: このセルをタップしたら、即ログアウトさせる関数を作成する
+            logout()
+            return
+        }
+        
+        navigationController?.pushViewController(destinationVC, animated: true)
+    }
+    
+    func logout(){
+        print("#ログアウトしました。（実際はされていない。後に実装）")
+    }
 }
