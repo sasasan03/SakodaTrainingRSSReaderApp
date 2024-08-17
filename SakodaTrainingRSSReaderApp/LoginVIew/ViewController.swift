e0a2d13726d6585158374348327db6ba06971c73
sasasan03 <sako.hiro62@gmail.com>
2024-08-17 17:00:14 +0900

    feat: バリデーションを実装。バリデーションに通らなければボタンが押せない仕様に変更。

--- SakodaTrainingRSSReaderApp/LoginVIew/ViewController.swift
+++ SakodaTrainingRSSReaderApp/LoginVIew/ViewController.swift
@@ -17,12 +17,29 @@ class ViewController: UIViewController {
     let activityIndicator = UIActivityIndicatorView(style: .large)
     var topics: [Topic]?
     var uid: UserID?
-    
+    let validation = Validation()
     @IBOutlet weak var inputMailTextField: UITextField!
     @IBOutlet weak var inputPasswordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        mailpasswordSignInButton.isEnabled = false
+        mailpasswordSignInButton.alpha = 1.0
+        mailpasswordSignInButton.setTitle("ログイン",for: .normal)
+        mailpasswordSignInButton.setTitleColor(UIColor.red, for: .disabled)
+        mailpasswordSignInButton.setTitleColor(UIColor.blue, for: .normal)
+        // メールの入力を検知
+        inputMailTextField.addTarget(
+            self,
+            action: #selector(mailAndPasswordTextFieldDidChange),
+            for: .editingChanged
+        )
+        // パスワードの入力を検知
+        inputPasswordTextField.addTarget(
+            self,
+            action: #selector(mailAndPasswordTextFieldDidChange),
+            for: .editingChanged
+        )
         do {
             self.uid = try userDefaultsManager.loadUserId()
             self.topics = try userDefaultsManager.loadTopics()
@@ -35,14 +52,18 @@ class ViewController: UIViewController {
         view.addSubview(activityIndicator)
     }
     
+    //メールとパスワードでログインするために使用するボタンとアクション。
+    @IBOutlet weak var mailpasswordSignInButton: UIButton!
     @IBAction func didTapMailPasswordSignInButton(_ sender: Any) {
         Task {
             do {
                 try await firebaseClient.mailPasswordSingIn(
-                    mail: inputMailTextField.text,
-                    password: inputPasswordTextField.text
+                    mail: inputMailTextField.text ?? "",
+                    password: inputPasswordTextField.text ?? ""
                 )
-                guard let topics = self.topics else { throw ViewControllerError.topicsNotFound }
+                guard let topics = self.topics else { 
+                    throw ViewControllerError.topicsNotFound
+                }
                 let feedListVC = FeedListViewController(topics: topics)
                 self.navigationController?.pushViewController(feedListVC, animated: true)
             }
@@ -52,8 +73,21 @@ class ViewController: UIViewController {
         }
     }
     
+    var isFormValid: Bool = false {
+        didSet {
+            mailpasswordSignInButton.isEnabled = isFormValid
+            if isFormValid {
+                mailpasswordSignInButton.setTitleColor(UIColor.blue, for: .normal)
+            } else {
+                mailpasswordSignInButton.setTitleColor(UIColor.red, for: .disabled)
+            }
+        }
+    }
+    
+    // Googleアカウントでログインするためのボタン。処理はextentionに記述。
     @IBOutlet weak var GoogleSignInButton: UIButton!
-        
+    
+    // サインアップの画面を表示させるためのボタンアクション。
     @IBAction func didTapMailSignUpViewButton(_ sender: Any) {
         let mailSignUpVC = MailSingUpViewController()
         mailSignUpVC.title = "新規登録画面"
@@ -61,17 +95,37 @@ class ViewController: UIViewController {
         navigationViewController.modalPresentationStyle = .fullScreen
         self.present(navigationViewController, animated: true)
     }
+    
 }
 
+//MARK: - ViewController内のエラーのenum。
 extension ViewController {
     
     enum ViewControllerError: Error {
         case topicsNotFound
         case userIDNotFound
     }
+    
 }
 
+//MARK: - textFieldのイベントを検知して実行されるメソッド。
 extension ViewController {
+    
+    @objc func mailAndPasswordTextFieldDidChange() {
+        let mailText = inputMailTextField.text ?? ""
+        let passText = inputPasswordTextField.text ?? ""
+        do {
+            isFormValid = try validation.isValidEmail(mailText) && validation.isValidPassword(passText)
+        } catch {
+            print("#ViewController error", error.localizedDescription)
+        }
+    }
+    
+}
+
+//MARK: - インジケータに関するメソッド。
+extension ViewController {
+    
     // インジケーターを表示にする
     private func showActivityIndicator() {
         activityIndicator.startAnimating()
@@ -83,7 +137,12 @@ extension ViewController {
         activityIndicator.stopAnimating()
         view.isUserInteractionEnabled = true
     }
-    // Googleアカウントでログイン
+    
+}
+
+//MARK: - Googleログインボタンに関するメソッド
+extension ViewController {
+
     // ２回目以降のログインの場合はRSSFeed一覧画面へ遷移する
     private func setupGoogleSignInButton() {
         GoogleSignInButton.addAction(UIAction { _ in
