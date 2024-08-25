4ee104130fe15b66cba9e70ceab99c76d3ccbb2b
sasasan03 <sako.hiro62@gmail.com>
2024-08-25 18:21:25 +0900

    feat: cellで文字サイズの通知を受け取るための処理を追加

--- SakodaTrainingRSSReaderApp/FeedListView/FeedListTableViewCell.swift
+++ SakodaTrainingRSSReaderApp/FeedListView/FeedListTableViewCell.swift
@@ -11,13 +11,30 @@ class FeedListTableViewCell: UITableViewCell {
     
     static let cellNibName = "FeedListTableViewCell"
     static let cellIdentifier = "FeedListCell"
+    var userDefaultsManager = UserDefaultsManager.shared
     
     @IBOutlet weak var feedText: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        NotificationCenter.default.addObserver(
+            self,
+            selector:  #selector(updateFontSize),
+            name: .fontSizeDidChange,
+            object: nil
+        )
+        // 保存されている文字サイズに合わせて文字の大きさが変更される
+        feedText.font = feedText.font.withSize(userDefaultsManager.fontSize)
     }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: .fontSizeDidChange, object: nil)
+    }
+    
+}
 
+extension FeedListTableViewCell {
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
@@ -25,6 +42,15 @@ class FeedListTableViewCell: UITableViewCell {
     func configureCellContent(item: Item){
         let title  = item.title
         feedText.text = title
+    }
+    
+}
+
+extension FeedListTableViewCell {
+    
+    @objc func updateFontSize() {
+        let updatedFontSize = userDefaultsManager.fontSize
+        feedText.font = feedText.font.withSize(updatedFontSize)
     }
     
 }
