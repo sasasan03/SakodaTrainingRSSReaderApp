f2c889b34128d9b328e65959f62ff246b066d860
sasasan03 <sako.hiro62@gmail.com>
2024-08-29 07:40:03 +0900

    move: CollectionViewを設定するため、新しいグループを追加

---
+++ SakodaTrainingRSSReaderApp/SettingsDetailView/ToggleListView/ToggleListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ToggleListViewController.swift
+//  SakodaTrainingRSSReaderApp
+//
+//  Created by sako0602 on 2024/08/09.
+//
+
+import UIKit
+
+class ToggleListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
